9ea2eac15d7fdd8e94954420f084f04e828e8aee
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-04-10 22:29:27 +0200

    Initial Setup

---
+++ ispexercice/src/test/java/edu/poniperro/AppTest.java
@@ -0,0 +1,12 @@
+package edu.poniperro;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertThat(true);
+    }
+}
